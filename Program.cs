4ea2fe5692cf1caafbea253aeacdf65c0aab5686
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-03-11 19:29:22 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("S");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("a");
+
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("n");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("t");
+
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write("o");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("s");
+
+
+        Console.ResetColor();
